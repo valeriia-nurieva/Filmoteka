af125a66ab316ad90d33e874b924dc2a7ae2e339
valeriia-nurieva <valerka.0906@gmail.com>
2022-10-27 16:43:33 +0300

    features/api and bugfix/grid

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 import { refs } from './js/refs';
+import { TmdbApi } from './js/TMDB-api';
+import { createMarkup } from './js/create-markup';
+
+import { Loading } from 'notiflix/build/notiflix-loading-aio';
+const loadingParams = {
+    svgColor: '#FF6B08',
+};
+
+const tmdb = new TmdbApi();
+
+initPage();
+
+async function initPage() {
+    try {
+        Loading.pulse(loadingParams);
+      const {results} = await tmdb.getTrendingMovies();
+      console.log(results);
+      const markup = createMarkup(results);
+      refs.listHome.insertAdjacentHTML('beforeend', markup);
+  } catch (error) {
+    console.log(error.message);
+  } finally {
+      Loading.remove();
+  }
+}
+
--- src/js/TMDB-api.js
+++ src/js/TMDB-api.js
@@ -1,2 +1,14 @@
 const axios = require('axios').default;
+const BASE_URL = 'https://api.themoviedb.org/3/';
 const API_KEY = '1d9e78535f6a01dcc41594da81e379a7';
+
+// 'https://api.themoviedb.org/3/trending/movie/day?api_key=1d9e78535f6a01dcc41594da81e379a7&adult=false'
+
+export class TmdbApi {
+
+    async getTrendingMovies() {
+        const { data } = await axios.get(`${BASE_URL}trending/movie/day?api_key=${API_KEY}&adult=false`);
+    return data;
+    }
+    
+}
