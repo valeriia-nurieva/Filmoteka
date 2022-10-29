5ac8b44252146e4b921e306b5012f4167f285392
SergioDegas <laferces@gmail.com>
2022-10-30 01:58:09 +0300

    new

--- src/js/FetchApi.js
+++ src/js/FetchApi.js
@@ -7,35 +7,26 @@ export default class FetchFilms {
   constructor() {
     this.page = 1;
     this.searchQuery = '';
-
   }
 
   /////Тренди
   async getTrendFilms() {
-   
-    
-      const { data } = await axios.get(
-        `${BASE_URL}trending/movie/day?api_key=${API_KEY}&adult=false`
-      );
-      return data;
-   
+    const { data } = await axios.get(
+      `${BASE_URL}trending/movie/day?api_key=${API_KEY}&adult=false`
+    );
+    return data;
   }
   /////пошук фильма
   async getFilmsByName() {
- 
-      const searchParams = new URLSearchParams({
-        api_key: API_KEY,
-        query: this.searchQuery,
-        language: 'en-US',
-        page: this.page,
-        include_adult: false, /// контент для дорослих и тд
-      });
-;
-      const { data } = await axios.get(
-        `${BASE_URL}search/movie?${searchParams}`
-      );
-      return data;
-    
+    const searchParams = new URLSearchParams({
+      api_key: API_KEY,
+      query: this.searchQuery,
+      language: 'en-US',
+      page: this.page,
+      include_adult: false, /// контент для дорослих и тд
+    });
+    const { data } = await axios.get(`${BASE_URL}search/movie?${searchParams}`);
+    return data;
   }
   ////Посилання на документацію для запиту повної інформації про кінофільм для сторінки кінофільму:
   async getFilmDetails(id) {
--- src/js/search.js
+++ src/js/search.js
@@ -1,20 +1,52 @@
 import FetchFilms from './FetchApi';
-
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox';
 import { createMarkup } from './create-markup';
 import { refs } from './refs';
+import { Notify } from 'notiflix';
 
 const fetch = new FetchFilms();
 const onSearchForm = document.querySelector('#search-form');
 onSearchForm.addEventListener('submit', onSearch);
 
+// body.style.backgroundColor = 'red';
+
+console.log(fetch);
 export default async function onSearch(e) {
   e.preventDefault();
 
-  fetch.searchQuery = e.currentTarget.elements.searchQuery.value;
+  fetch.searchQuery = e.currentTarget.elements.searchQuery.value.trim();
 
-  const { results } = await fetch.getFilmsByName();
-//   console.log(results);
-//   console.log(results);
+  //   console.log(results);
+  //   console.log(results);
+
+  try {
+    if (fetch.searchQuery === '') {
+      Notify.failure('Please enter your search data.');
+    } else {
+      const { results, total_pages } = await fetch.getFilmsByName();
+
+      console.log(total_pages);
+      if (results.length === 0) {
+        setTimeout(
+          Notify.info(
+            'Sorry, there are no images matching your search query. Please try again.'
+          ),
+          0
+        );
+      } else {
+        refs.listHome.innerHTML = '';
+
+        createMarkup(results);
+      }
+    }
+  } catch (error) {
+    Notify.failure(
+      "We're sorry, but you've reached the end of search results."
+    );
+  }
+}
+
+function clearList() {
   refs.listHome.innerHTML = '';
-  createMarkup(results);
 }
