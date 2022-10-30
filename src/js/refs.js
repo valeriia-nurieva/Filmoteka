5242261952e5f4ca06ddf4f9266f4dee899e2423
valeriia-nurieva <valerka.0906@gmail.com>
2022-10-30 17:39:33 +0200

    fixed refs

--- src/js/refs.js
+++ src/js/refs.js
@@ -2,7 +2,6 @@ export const refs = {
   listHome: document.querySelector('.film-list-home-js'),
   listLib: document.querySelector('.film-list-lib-js'),
   backdrop: document.querySelector('.backdrop'),
-  card: document.querySelector('.list'),
   createData: document.querySelector('.create-data'),
   modalCloseBtn: document.querySelector('.modal__close'),
 };
