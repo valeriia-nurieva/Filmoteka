d1bb898da3bc2ff06c94c7cb1051917e783846a6
Ivan <gubarenkovanek@gmail.com>
2022-10-31 13:40:27 +0200

    edited
    made markup for lib-back,create scss file for style.add js-script

--- src/library.js
+++ src/library.js
@@ -1,2 +1,19 @@
 import './js/render-library';
 import './js/btn-back-to-top';
+
+const watchedBtn = document.querySelector('.watched');
+const queueBtn = document.querySelector('.queue');
+
+function onWatched() {
+    watchedBtn.classList.add('js-active');
+    queueBtn.classList.remove('js-active');
+}
+function onQueueBtn() {
+    queueBtn.classList.add('js-active');
+    watchedBtn.classList.remove('js-active');
+}
+
+watchedBtn.addEventListener('click', onWatched);
+queueBtn.addEventListener('click', onQueueBtn);
+
+
