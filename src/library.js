77d37bf0b1f132688b314e0b6716feeeb4d12c48
valeriia-nurieva <valerka.0906@gmail.com>
2022-10-31 23:45:31 +0200

    fix card styles for dark theme

--- src/library.js
+++ src/library.js
@@ -17,6 +17,36 @@ function onQueueBtn() {
 
 watchedBtn.addEventListener('click', onWatched);
 queueBtn.addEventListener('click', onQueueBtn);
+const body = document.querySelector('.page');
 
 
+function addBgc() {
+        body.style.backgroundColor = '#FFF';
+        body.classList.remove('color');
+}
+
+function removeBgc() {
+    body.classList.add('color');
+    body.style.backgroundColor = 'rgba(0, 0, 0, 0.74)';
+}
+let DARK = 'DARK';
+let LIGHT = 'LIGHT';
+dark();
+
+function dark() {
+  if (!localStorage.getItem(DARK)) {
+      return;
+    } 
+    localStorage.getItem(DARK);
+    removeBgc();
+};
+
+light();
+
+function light() {
+  if (!localStorage.getItem(LIGHT)) {
+    return;
+    }addBgc();
+    localStorage.getItem(LIGHT);
+};
 
