8c5edaab2dbf56b1bc766d88eddc6b4e20a2286b
SergioDegas <laferces@gmail.com>
2022-11-01 18:33:02 +0200

    fixSearch

--- src/js/search.js
+++ src/js/search.js
@@ -4,12 +4,11 @@ import SimpleLightbox from 'simplelightbox';
 import { createMarkup } from './create-markup';
 import { refs } from './refs';
 import { Notify } from 'notiflix';
-
+import { onPaginationClick } from './pagination';
 const fetch = new FetchFilms();
 const onSearchForm = document.querySelector('#search-form');
 onSearchForm.addEventListener('submit', onSearch);
 const searchError = document.querySelector('.search-error');
-
 
 searchError.textContent = '';
 
@@ -18,34 +17,40 @@ export default async function onSearch(e) {
 
   fetch.searchQuery = e.currentTarget.elements.searchQuery.value.trim();
 
-
   try {
     if (fetch.searchQuery === '') {
       searchError.textContent = 'Please enter your search data.';
     } else {
-      const { results} = await fetch.getFilmsByName();
+      const { results, total_results } = await fetch.getFilmsByName();
 
-      if (results.length === 0) {
+      if (fetch.query === 0) {
         setTimeout(
           (searchError.textContent =
             'Search result not successful. Enter the correct movie name and'),
           0
         );
       } else {
+          if (total_results === 0) {
+            setTimeout(
+              (searchError.textContent =
+                'Search result not successful. Enter the correct movie name and'),
+              0
+            );
+          } else {
+             clearList();
 
-      clearList();
-
-        createMarkup(results);
+             createMarkup(results);
+          }
+       
+        // onPaginationClick(total_pages);
       }
     }
   } catch (error) {
-    searchError.textContent ="We're sorry, but you've reached the end of search results.";
+  
   }
 }
 
 function clearList() {
   refs.listHome.innerHTML = '';
-        searchError.textContent = '';
-
+  searchError.textContent = '';
 }
-
