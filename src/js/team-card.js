30ddcb7a8324c021c0b3a9d9933a37510877be72
Lidiia Popova <pl.95kh@gmail.com>
2022-11-02 19:15:48 +0200

    update css

---
+++ src/js/team-card.js
@@ -0,0 +1,80 @@
+import valeriaNurieva from '../images/team/hacker-3342696_640.jpg';
+import serhiiFetsyk from '../images/team/hacker-3342696_640.jpg';
+import maksymBiesieda from '../images/team/hacker-3342696_640.jpg';
+import vadymNagornyi from '../images/team/hacker-3342696_640.jpg';
+import lidiyaPopova from '../images/team/hacker-3342696_640.jpg';
+import marynaOrel from '../images/team/hacker-3342696_640.jpg';
+import vladyslavBlyznyuk from '../images/team/hacker-3342696_640.jpg';
+import anitaZarichna from '../images/team/hacker-3342696_640.jpg';
+import ivanHubarenko from '../images/team/hacker-3342696_640.jpg';
+import vladyslavDanylchenko from '../images/team/hacker-3342696_640.jpg';
+import oleksiiBiesieda from '../images/team/hacker-3342696_640.jpg';
+
+export const teamItems = [
+  {
+    img: valeriaNurieva,
+    name: 'Valeria Nurieva',
+    role: 'Team Lead',
+    git: 'https://github.com/valeriia-nurieva',
+  },
+  {
+    img: serhiiFetsyk,
+    name: 'Serhiy Fetsyk',
+    role: 'Scrum Master',
+    git: 'https://github.com/SergioDegas',
+  },
+  {
+    img: maksymBiesieda,
+    name: 'Maksym Biesieda',
+    role: 'Developer',
+    git: 'https://github.com/MaksymBiesieda',
+  },
+  {
+    img: vadymNagornyi,
+    name: 'Vadym Nagornyi',
+    role: 'Developer',
+    git: 'https://github.com/Vadym8177',
+  },
+  {
+    img: lidiyaPopova,
+    name: 'Lidiya Popova',
+    role: 'Developer',
+    git: 'https://github.com/lidiya-p',
+  },
+  {
+    img: vladyslavBlyznyuk,
+    name: 'Vladyslav Blyznyuk',
+    role: 'Developer',
+    git: 'https://github.com/VladislavBVV',
+  },
+  {
+    img: marynaOrel,
+    name: 'Maryna Orel',
+    role: 'Developer',
+    git: 'https://github.com/Maryna-Orel',
+  },
+  {
+    img: anitaZarichna,
+    name: 'Anita Zarichna',
+    role: 'Developer',
+    git: 'https://github.com/AnitaZarichna',
+  },
+  {
+    img: ivanHubarenko,
+    name: 'Ivan Hubarenko',
+    role: 'Developer',
+    git: 'https://github.com/Hubarenko10',
+  },
+  {
+    img: vladyslavDanylchenko,
+    name: 'Vladyslav Danylchenko',
+    role: 'Developer',
+    git: 'https://github.com/Vlad-Danilchenko',
+  },
+  {
+    img: oleksiiBiesieda,
+    name: 'Oleksii Biesieda',
+    role: 'Developer',
+    git: 'https://github.com/OleksiiBiesieda',
+  },
+];
