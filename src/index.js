b271b04c6567a01103a79b87c3a5e0436db03cea
valeriia-nurieva <valerka.0906@gmail.com>
2022-11-02 18:50:35 +0200

    test-pagination

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import './js/btn-back-to-top';
 import pagination from './js/pagination';
 import './js/modal-main';
 
+
 const pagJs = document.querySelector('.pagJs');
 
 import FetchFilms from './js/FetchApi';
 import { Loading } from 'notiflix/build/notiflix-loading-aio';
 const fetch = new FetchFilms();
 const pagPage = document.querySelector('.pagination');
-
 
 
 const loadingParams = {
@@ -26,11 +26,10 @@ initPage();
 async function initPage() {
   try {
     Loading.pulse(loadingParams);
-    const { results, total_results, page, total_pages } =
+    const { results, page, total_pages } =
       await fetch.getTrendFilms();
-    
+    pagination(page, total_pages);
     createMarkup(results);
-    clickPagin()
   } catch (error) {
     console.log(error.message);
   } finally {
@@ -38,35 +37,64 @@ async function initPage() {
   }
 }
 
+const paginationBox = document.querySelector('.pagination');
+paginationBox.addEventListener('click', handlerPagination);
 
-pagPage.addEventListener('click', clickPagin);
-async function clickPagin() {
-  let CurrentPage = 1;
- 
-  try {
-    Loading.pulse(loadingParams);
-
+async function handlerPagination(evt) {
+  if (evt.target.nodeName !== 'LI') {
+    return
+  }
+  if (evt.target.textContent === "🡸") {
+   
+    return;
+  }
+  if (evt.target.textContent === "🡺") {
+   
+    return;
+  }
+  if (evt.target.textContent === "...") {
+    return
+  }
+  let pages = evt.target.textContent
+      console.log(pages);
+  fetch.pages = pages;
+  console.log('fetch.pages', fetch.pages);
     const { results, total_results, page, total_pages } =
-      await fetch.getTrendFilms();
+    await fetch.getTrendFilms();
+  console.log(results);
+  refs.listHome.innerHTML = ''; //очищає бокс з картками
+  pagJs.innerHTML = ''; //очищає пагінацію
+  createMarkup(results);
+}
 
-      pagJs.innerHTML = '';
-       if ((CurrentPage = page)) {
-         pagination(CurrentPage, total_pages);
+// pagPage.addEventListener('click', clickPagin);
+// async function clickPagin() {
+//   let CurrentPage = 1;
+ 
+//   try {
+//     Loading.pulse(loadingParams);
+
+//     const { results, total_results, page, total_pages } =
+//       await fetch.getTrendFilms();
+
+//       pagJs.innerHTML = '';
+//        if ((CurrentPage = page)) {
+//          pagination(CurrentPage, total_pages);
       
        
-         fetch.incrementPage();
-         console.log(CurrentPage);
-         console.log(pagPage);
+//          fetch.incrementPage();
+//          console.log(CurrentPage);
+//          console.log(pagPage);
 
-         createMarkup(results);
-       }
-  } catch (error) {
-    console.log(error.message);
-  } finally {
-    Loading.remove();
-  }
-  // clearList();
-}
+//          createMarkup(results);
+//        }
+//   } catch (error) {
+//     console.log(error.message);
+//   } finally {
+//     Loading.remove();
+//   }
+//   // clearList();
+// }
 
 import onSearch from './js/search';
 import changeColor from './js/svgBgc';
